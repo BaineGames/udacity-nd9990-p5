4cd712e83691573025ff613c17708f95066e6ec9
BaineGames <noah.baine@gmail.com>
2019-10-01 11:39:43 -0700

    add gettodos function modeled after udagram lesson videos

---
+++ src/lambda/http/getTodos.ts
@@ -0,0 +1,28 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import 'source-map-support/register'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    
+    console.log("EVENT:", event);
+    
+    const result = await docClient.scan({
+        TableName: todosTable
+    }).promise()
+
+    const items = result.Items
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            items
+        })
+    }
+}
